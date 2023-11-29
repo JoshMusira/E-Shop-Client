e9b5583f4ef2f1ebc715fb50b162aeff8d717546
JoshMusira <joshuamusira01@gmail.com>
2023-11-29 22:26:53 +0300

    feat:update homepage

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,10 +3,21 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(URL);
-  console.log(res)
+  try {
+    const res = await fetch(URL);
+    console.log(res)
 
-  return res.json();
+    if (!res.ok) {
+      throw new Error('Failed to fetch categories');
+    }
+
+    const data = await res.json();
+    console.log('Data received:', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    return []; // Return an empty array in case of an error
+  }
 };
 
 export default getCategories;
